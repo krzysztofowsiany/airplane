fb66562f1acf00d2abf106598ae6e9225f9d08ca
gocom <k.owsiany@hydro-partner.pl>
2014-03-05 12:42:12 +0100

    add animation

--- Game/Airplane.js
+++ Game/Airplane.js
@@ -8,6 +8,7 @@ function Airplane() {
 		game = Phaser.Game,
 		map,
 		coins,
+		bomb,
 		layer = undefined,
 		sprite = undefined,
 		cursors = undefined,
@@ -30,31 +31,37 @@ function Airplane() {
 	    game.load.image('tmw_desert_spacing', 'tmw_desert_spacing.png');
 
 	    game.load.image('phaser', 'arrow.png');
-	   // game.load.spritesheet('coin', 'coin.png', 32, 32);
+	    game.load.spritesheet('coin', 'coin.png', 32, 32);
 	    
     }
 	
 	
+	function dropBomb(x,y){
+		bomb = game.add.sprite(x, y, 'coin');
+		bomb.anchor.setTo(0.5, 0.5);
+		bomb.body.setRectangle(16, 16, 25, 15);
+		bomb.rotation = Math.PI / 2;
+		//bomb.body.maxAngular = 100;
+	    //bomb.body.angularDrag = 100;
+	    bomb.body.collideWorldBounds = true;
+	    bomb.animations.add('walk');
+	    bomb.animations.play('walk', 20, true);
+
+	}
 	
 	function create () {
 		
 	    map = game.add.tilemap('map');
-	    
-	
 	    map.addTilesetImage('tmw_desert_spacing');
-	    
-	    
-	    map.setCollisionBetween(1, 12);
-
-
+	    map.setCollisionBetween(1, 10);
 	    map.setTileIndexCallback(10, hitCoin, this);
 	    //map.setTileLocationCallback(2, 0, 1, 1, hitCoin, this);
-
+	    //map.replace(10, 11);//s,tile.tile.x, tile.tile.y);
 	    layer = map.createLayer('Tile Layer 1');
 
 	    layer.resizeWorld();
 
-
+	    
 	    sprite = game.add.sprite(0, 100, 'phaser');
 	    sprite.anchor.setTo(0.5, 0.5);
 
@@ -71,28 +78,33 @@ function Airplane() {
 	   // game.camera.follow(sprite);
 
 	   // cursors = game.input.keyboard.createCursorKeys();
-	    
+	    jumpButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+	    dropBomb(100,0);
 	}
 
 	function hitCoin(sprite, tile) {
-	//	console.log(tile);
+		console.log(tile);
+		//map.replace(10, 11);//s,tile.tile.x, tile.tile.y);
 		map.putTile(0, tile.tile.x, tile.tile.y, layer);
 	    //tile.tile.alpha = 0;
-		
+		//map.setTileIndexCallback(10, hitCoin, this);
 	    //tile.tile.
+		
+	  //  layer.dirty = true;
 
-//	    layer.dirty = true;
-
-	    return true;
+	    return false;
 
 	}
 
 	
 	function update() {
 		game.physics.collide(sprite, layer);
-
+		//game.physics.overlap(sprite, layer);
+		
         game.physics.velocityFromAngle(sprite.angle, 30, sprite.body.velocity);
        
+        if (bomb != undefined)
+        	game.physics.velocityFromAngle(bomb.angle, 30, bomb.body.velocity);
 	}
 
 
